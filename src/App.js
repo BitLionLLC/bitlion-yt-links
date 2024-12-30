69b8135d9ae8aa5388e86f1d6fff1c89da590db9
Grant Reighard <grant.reighard@gmail.com>
2024-12-29 19:25:43 -0500

    Fix errors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import "./App.css";
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "./components/header";
 import LinkList from "./components/linkList";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 
-import backgroundImage1 from "./assets/app-background1.jpg";
-import backgroundImage2 from "./assets/app-background2.jpg";
-import backgroundImage3 from "./assets/app-background3.jpg";
-import backgroundImage4 from "./assets/app-background4.jpg";
-
-const backgrounds = [
-  backgroundImage1,
-  backgroundImage2,
-  backgroundImage3,
-  backgroundImage4,
-];
 
 function App() {
   return (

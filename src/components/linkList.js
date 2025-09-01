22e851acbb1ac474e19d037ca5425f2e0761aaaf
Grant Reighard <grant.reighard@gmail.com>
2025-09-01 12:36:25 -0400

    clean up links and add TripleDev

--- src/components/linkList.js
+++ src/components/linkList.js
@@ -5,8 +5,14 @@ import googlePlayBadge from "../assets/google_play.png";
 
 const links = [
   {
+    link: "https://www.tripledev.co",
+    title: "TripleDev Software Development Agency",
+    icon: ["fas", "code"],
+    color: "black",
+  },
+  {
     link: "https://www.fluxtrade.net",
-    title: "FluxTrade - Futures trading system (indicators and strategies)",
+    title: "FluxTrade - NinjaTrader automated strategies",
     icon: ["fas", "dollar-sign"],
     color: "green",
   },
@@ -14,7 +20,7 @@ const links = [
     link: "https://www.etsy.com/shop/DesignByBitLion",
     title: "Our Etsy shop",
     icon: ["fas", "cart-shopping"],
-    color: "green",
+    color: "blue",
   },
   {
     link: "",
@@ -27,102 +33,6 @@ const links = [
     title: "Astral Tarot Reader - Read your tarot card photos",
     icon: <div><a href="https://play.google.com/store/apps/details?id=com.thebitlion.astraltarotreader"><img src={googlePlayBadge} width={150} alt="Google Play Icon" /></a></div>,
     color: "green",
-  },
-  {
-    link: "https://invideo.sjv.io/jrQK40",
-    title: "Create videos like ours",
-    icon: ["fas", "video"],
-    color: "black",
-  },
-  {
-    link: "https://namecheap.pxf.io/NKgy2b",
-    title: "Get a domain for your project",
-    icon: ["fas", "globe"],
-    color: "black",
-  },
-  {
-    link: "https://ssls.sjv.io/xLJe9x",
-    title: "Get an SSL for your website",
-    icon: ["fas", "lock"],
-    color: "black",
-  },
-  {
-    link: "https://easyship.ilbqy6.net/2aRo10",
-    title: "Simplify your package shipments",
-    icon: ["fas", "box"],
-    color: "black",
-  },
-  {
-    link: "https://godlikehost.sjv.io/7aXE5Y",
-    title: "Start a gaming server",
-    icon: ["fas", "gamepad"],
-    color: "black",
-  },
-  {
-    link: "https://united.elfm.net/DyWYK5",
-    title: "Earn frequent flier miles",
-    icon: ["fas", "plane"],
-    color: "black",
-  },
-  {
-    link: "https://www.youtube.com/@TheMarketMatrixTV",
-    title: "The Market Matrix",
-    icon: ["fab", "youtube"],
-    color: "red",
-  },
-  {
-    link: "https://www.youtube.com/@BizarreFactsTV",
-    title: "Bizarre Facts",
-    icon: ["fab", "youtube"],
-    color: "red",
-  },
-  {
-    link: "https://www.youtube.com/@FamousRealQuotes",
-    title: "Famous Real Quotes",
-    icon: ["fab", "youtube"],
-    color: "red",
-  },
-  {
-    link: "https://www.youtube.com/@ChillingStoriesTV",
-    title: "Chilling Stories",
-    icon: ["fab", "youtube"],
-    color: "red",
-  },
-  {
-    link: "https://www.youtube.com/@LifeLogicTV",
-    title: "Life Logic",
-    icon: ["fab", "youtube"],
-    color: "red",
-  },
-  {
-    link: "https://www.youtube.com/@HistoryUnfoldedTV",
-    title: "History Unfolded",
-    icon: ["fab", "youtube"],
-    color: "red",
-  },
-  {
-    link: "https://www.youtube.com/@EnglishMinuteTV",
-    title: "English Minute",
-    icon: ["fab", "youtube"],
-    color: "red",
-  },
-  {
-    link: "https://www.youtube.com/@MysteryMinutesTV",
-    title: "Mystery Minutes",
-    icon: ["fab", "youtube"],
-    color: "red",
-  },
-  {
-    link: "https://www.youtube.com/@ParadoxPulseTV",
-    title: "Paradox Pulse",
-    icon: ["fab", "youtube"],
-    color: "red",
-  },
-  {
-    link: "https://www.youtube.com/@AncientGeniusTV",
-    title: "Ancient Genius",
-    icon: ["fab", "youtube"],
-    color: "red",
   },
 ];
 

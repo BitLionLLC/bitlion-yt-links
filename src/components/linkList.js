39cc8d0e5a33774938ba108b9b3b4e6d2c52a10c
Grant Reighard <grant.reighard@gmail.com>
2026-03-05 18:58:47 -0500

    adds stan store link

--- src/components/linkList.js
+++ src/components/linkList.js
@@ -5,6 +5,18 @@ import googlePlayBadge from "../assets/google_play.png";
 
 const links = [
   {
+    link: "https://stan.store/affiliates/514cd9ae-ed8f-4653-b008-8a2889b7d79b",
+    title: "The Digital Marketing Mini-Course - Learn how we make $8k/mo",
+    icon: ["fas", "video"],
+    color: "red",
+  },
+  {
+    link: "https://stan.store/bitlion/?utm_source=bitlionus",
+    title: "Our Stan Store - Learn how to escape your 9-5",
+    icon: ["fas", "store"],
+    color: "black",
+  },
+  {
     link: "https://www.fluxtrade.net/?utm_source=bitlionus",
     title: "FluxTrade trading strategies (new affiliate program!)",
     icon: ["fas", "dollar-sign"],
@@ -15,12 +27,6 @@ const links = [
     title: "Our Etsy shop",
     icon: ["fas", "cart-shopping"],
     color: "blue",
-  },
-  {
-    link: "https://stan.store/affiliates/514cd9ae-ed8f-4653-b008-8a2889b7d79b",
-    title: "The Digital Marketing Mini-Course",
-    icon: ["fas", "video"],
-    color: "red",
   },
 ];
 

1003c85c1c93dfd9a21b32db025c5fd063150028
Grant Reighard <grant.reighard@gmail.com>
2026-04-13 21:23:18 -0400

    Adds 2 new affiliate links

--- src/components/linkList.js
+++ src/components/linkList.js
@@ -10,18 +10,6 @@ const links = [
     icon: ["fas", "link"],
     color: "black",
   },
-  // {
-  //   link: "https://stan.store/affiliates/514cd9ae-ed8f-4653-b008-8a2889b7d79b",
-  //   title: "The Digital Marketing Mini-Course - Learn how we make $8k/mo",
-  //   icon: ["fas", "video"],
-  //   color: "red",
-  // },
-  // {
-  //   link: "https://stan.store/bitlion/?utm_source=bitlionus",
-  //   title: "Our Stan Store - Learn how to escape your 9-5",
-  //   icon: ["fas", "store"],
-  //   color: "black",
-  // },
   {
     link: "https://www.fluxtrade.net/?utm_source=bitlionus",
     title: "FluxTrade trading strategies (new affiliate program!)",
@@ -46,6 +34,24 @@ const links = [
     icon: <div><a href="https://apps.apple.com/us/app/budget-plant-id/id6747782540"><img src={appStoreIcon} alt="App Store Icon" width={150} /></a><a href="https://play.google.com/store/apps/details?id=com.thebitlion.plantidentifierflutter"><img src={googlePlayBadge} width={150} alt="Google Play Icon" /></a></div>,
     color: "green",
   },
+  {
+    link: "https://stan.store/affiliates/514cd9ae-ed8f-4653-b008-8a2889b7d79b",
+    title: "The Digital Marketing Mini-Course - Learn how we make $8k/mo",
+    icon: ["fas", "video"],
+    color: "red",
+  },
+  {
+    link: "https://millionairepartnership.com/webclass-d24#aff=blauente",
+    title: "Millionaire Partner System - Make $3k/day on autopilot",
+    icon: ["fas", "gem"],
+    color: "blue",
+  },
+  // {
+  //   link: "https://stan.store/bitlion/?utm_source=bitlionus",
+  //   title: "Our Stan Store - Learn how to escape your 9-5",
+  //   icon: ["fas", "store"],
+  //   color: "black",
+  // },
 ];
 
 const LinkList = ({ isLive }) => {
